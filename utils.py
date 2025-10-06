d8c2dd57882345c5610648775103e5e481f5650b
Brian Delaney <briandelaney2028@u.northwestern.edu>
2025-10-06 13:44:38 -0500

    Added temporal option for training loading

--- utils.py
+++ utils.py
@@ -6,16 +6,19 @@ import glob
 
 DATA_DIR = 'Data/'
 
-def load_training_df(method:str='inner')->pd.DataFrame:
+def load_training_df(method:str='inner', temporal:bool=False)->pd.DataFrame:
     """
     Loads and merges all training input and output CSV files from the Data/train directory into a single DataFrame.
 
     Parameters:
         method (str): Merge method for pandas.merge (e.g., 'inner', 'left').
             'inner' retains only matched keys, 'left' retains all rows from input.
+        temporal (bool):
+            If True, merges input and output data on ['game_id', 'play_id', 'nfl_id', 'frame_id'] (frame-level, temporal join).
+            If False, aggregates input data to the last frame per ['game_id', 'play_id', 'nfl_id'] before merging on those keys (non-temporal, play-level join).
 
     Returns:
-        pd.DataFrame: Merged DataFrame containing input features and true output values.
+        pd.DataFrame: Merged DataFrame containing input features and true output values, either at the frame or play level depending on 'temporal'.
 
     Raises:
         FileNotFoundError: If no input files are found in the expected directory.
@@ -32,13 +35,22 @@ def load_training_df(method:str='inner')->pd.DataFrame:
     df_input  = pd.concat([pd.read_csv(f) for f in input_files])
     df_output = pd.concat([pd.read_csv(f) for f in output_files])
 
-    df_output = df_output.rename(columns={'x':'x_true', 'y':'y_true'})
-    ### how = 'inner' -> inner vector product only retains matched keys
-    ### how = 'left'  -> will retain all rows in df_input
-    df_merged = pd.merge(
-        df_input, df_output,
-        on=['game_id', 'play_id', 'nfl_id', 'frame_id'], how=method
-    )
+    if temporal:
+        df_merged = pd.merge(
+            df_input, df_output,
+            on=['game_id', 'play_id', 'nfl_id', 'frame_id'], 
+            how=method,
+            suffixes=('_input', '_target')
+        )
+    else:
+        input_grouped = df_input.groupby(['game_id', 'play_id', 'nfl_id']).last().reset_index()
+        input_grouped = input_grouped.drop('frame_id', axis=1)
+        df_merged = pd.merge(
+            input_grouped, df_output,
+            on=['game_id', 'play_id', 'nfl_id'], 
+            how=method,
+            suffixes=('_input', '_target')
+        )
     return df_merged
 
 def load_supplemental_df(method:str='inner')->pd.DataFrame:
@@ -78,8 +90,8 @@ def height_to_inches(height_str: str) -> int:
         return np.nan
 
 if __name__=='__main__':
-    df_train = load_training_df()
-    print(df_train.head())
+    df_train = load_training_df(temporal=False)
+    print(df_train.loc[:20, ['frame_id', 'x_input', 'x_target']])
 
-    df_supp = load_supplemental_df()
-    print(df_supp.head())
+    # df_supp = load_supplemental_df()
+    # print(df_supp.head())
